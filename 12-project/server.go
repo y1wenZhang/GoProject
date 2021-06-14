f3e08272fd1e38e0c40a55272b61dfe3cae0de63
zhanghang <y1wenzhang@yeah.net>
2021-06-14 23:53:33 +0800

    IM在线聊天系统，1.基础server构建

---
+++ 12-project/server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+}
+
+func (s *Server) NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:   ip,
+		Port: port,
+	}
+	return server
+}
+
+func (s *Server) Handler(conn net.Conn) {
+	fmt.Println("连接服务器成功")
+}
+
+func (s *Server) Start() {
+	// socket listen
+	listener, err := net.Listen("tcp4", fmt.Sprintf("%s:%d", s.Ip, s.Port))
+	if err != nil {
+		fmt.Println("net listern error: ", err)
+	}
+	// listen close
+	defer listener.Close()
+	for {
+		// accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept error: ", err)
+			continue
+		}
+		// do handler
+		s.Handler(conn)
+	}
+
+}
