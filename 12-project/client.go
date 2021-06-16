649790c47611845d03d68bdcea45ff82590dede0
zhanghang <y1wenzhang@yeah.net>
2021-06-16 15:20:05 +0800

    即时通讯系统，8-客户端，命令行解析

--- 12-project/client.go
+++ 12-project/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -28,8 +29,17 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+var serverIp string
+var serverPort int
+
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器IP地址(默认是127.0.0.1)")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口(默认是8888)")
+}
+
 func main() {
-	client := NewClient("127.0.0.1", 8888)
+	flag.Parse()
+	client := NewClient(serverIp, serverPort)
 	if client == nil {
 		fmt.Println(">>>>>>>>>CONNECT SERVER FAILED<<<<<<<<<<")
 	}
