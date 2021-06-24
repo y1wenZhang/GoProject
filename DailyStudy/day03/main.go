9325449c02d837707093c1bd86366438bf13acc7
zhanghang <y1wenzhang@yeah.net>
2021-06-25 00:02:16 +0800

    DailyStudy day03

---
+++ DailyStudy/day03/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// 1.
+func main() {
+	s := make([]int, 5)
+	s = append(s, 1, 2, 3)
+	fmt.Println(s)
+}
+
+// 2.
+/*
+func main() {
+	s := make([]int,0)
+	s = append(s,1,2,3,4)
+	fmt.Println(s)
+}
+*/
