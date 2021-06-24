3172409f17595a403d53e562ce537259c923fa7e
zhanghang <y1wenzhang@yeah.net>
2021-06-24 23:49:30 +0800

    add get url

---
+++ GoProgramDesign/1-入门/get_url/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		fmt.Println(url)
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		}
+		fmt.Println(resp.Status)
+		//io.Copy(os.Stdout, resp.Body)
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s", b)
+	}
+}
